215b70576bfbe280a120a146301fafdd201c42eb
Jonathan Hahn <Jonathan.Hahn@sensirion.com>
2018-08-13 16:27:11 +0200

    Change style of code
    code uniformly indented

--- examples/sdp8xx/sdp8xx.ino
+++ examples/sdp8xx/sdp8xx.ino
@@ -11,29 +11,29 @@ void setup() {
 
   int ret = sdp.init();
   if (ret == 0) {
-      Serial.print("init(): success\n");
+    Serial.print("init(): success\n");
   } else {
-      Serial.print("init(): failed, ret = ");
-      Serial.println(ret);
-      while (true) {
-        delay(1000);
-      }
+    Serial.print("init(): failed, ret = ");
+    Serial.println(ret);
+    while (true) {
+      delay(1000);
+    }
   }
 }
 
 void loop() {
   int ret = sdp.readSample();
   if (ret == 0) {
-      Serial.print("Differential pressure: ");
-      Serial.print(sdp.getDifferentialPressure());
-      Serial.print("Pa | ");
+    Serial.print("Differential pressure: ");
+    Serial.print(sdp.getDifferentialPressure());
+    Serial.print("Pa | ");
 
-      Serial.print("Temp: ");
-      Serial.print(sdp.getTemperature());
-      Serial.print("C\n");
+    Serial.print("Temp: ")
+    Serial.print(sdp.getTemperature());
+    Serial.print("C\n");
   } else {
-      Serial.print("Error in readSample(), ret = ");
-      Serial.println(ret);
+    Serial.print("Error in readSample(), ret = ")
+    Serial.println(ret);
   }
 
   delay(500);
